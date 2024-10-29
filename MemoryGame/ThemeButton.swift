deb203f710834880b38beb27df535fff5b55b0a0
MarcinJ0 <s95419@pollub.edu.pl>
2024-10-29 13:11:22 +0100

    lab3 changes

---
+++ MemoryGame/ThemeButton.swift
@@ -0,0 +1,18 @@
+//
+//  ThemeButton.swift
+//  MemoryGame
+//
+//  Created by student on 29/10/2024.
+//
+
+import SwiftUI
+
+struct ThemeButton: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ThemeButton()
+}
